dba5b6f68074398ac5ba286cde4e098869b0e6e9
Kevin Ramos <kevin@Kevins-MacBook-Pro.local>
2018-12-05 12:46:40 -0800

    Added module and script directories

---
+++ scripts/my_script.py
@@ -0,0 +1,12 @@
+"""Script to run my program."""
+
+# This adds the directory above to our Python path
+# This is so that we can import our custom python module code into this script
+
+import sys
+sys.path.append('../')
+
+# Imports
+from my_module.functions import my_func
+
+
