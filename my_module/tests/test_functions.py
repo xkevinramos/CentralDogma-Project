151dc096aa83769c3048f5db5ed27cd2b1ee21ba
Kevin Ramos <kevin@Kevins-MacBook-Pro.local>
2018-12-05 22:33:57 -0800

    Fixed import

--- my_module/tests/test_functions.py
+++ my_module/tests/test_functions.py
@@ -1,6 +1,6 @@
-from functions import complement, transcribe, translate
 import sys
 sys.path.append('../')
+from functions import complement, transcribe, translate
 
 test_template_strand = 'ATCTGACC'
 test_dna_strand = 'TTCCATA'
@@ -8,23 +8,23 @@ test_rna_strand = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
 
 def test_complement():
     print("Testing complement function...")
-    assert isinstance(complement(test_template_strand), string)
+    assert isinstance(complement(test_template_strand), str)
     assert complement(test_template_strand) == 'TAGACTGG'
 
 
 def test_transcribe():
     print("Testing transcribe function...")
-    assert isinstance(transcribe(test_dna_strand), string)
+    assert isinstance(transcribe(test_dna_strand), str)
     assert transcribe(test_dna_strand) == 'TTCCUTU'
 
 
 def test_translate():
     print("Testing translate function...")
-    assert isinstance(translate(test_rna_strand), string)
+    assert isinstance(translate(test_rna_strand), str)
     assert translate(test_rna_strand) == 'MAMAPRTEINSTRING'
 
 
 test_complement()
 test_transcribe()
-test_translation()
+test_translate()
 
