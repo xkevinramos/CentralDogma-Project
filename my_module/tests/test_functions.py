8a401feeabd87d289ee3c7150c1e536c22c4b9b1
Kevin Ramos <kevin@wireless-169-228-76-220.ucsd.edu>
2018-12-08 01:46:02 -0800

    Test files

--- my_module/tests/test_functions.py
+++ my_module/tests/test_functions.py
@@ -10,18 +10,21 @@ def test_complement():
     print("Testing complement function...")
     assert isinstance(complement(test_template_strand), str)
     assert complement(test_template_strand) == 'TAGACTGG'
+    print("All tests passed!\n")
 
 
 def test_transcribe():
     print("Testing transcribe function...")
     assert isinstance(transcribe(test_dna_strand), str)
     assert transcribe(test_dna_strand) == 'TTCCUTU'
+    print("All tests passed!\n")
 
 
 def test_translate():
     print("Testing translate function...")
     assert isinstance(translate(test_rna_strand), str)
     assert translate(test_rna_strand) == 'MAMAPRTEINSTRING'
+    print("All tests passed!\n")
 
 
 test_complement()
